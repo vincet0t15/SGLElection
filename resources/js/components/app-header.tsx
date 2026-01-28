cd243447e0ca63ced1cb3d4be6bc5c49cab19413
vincet0t15 <vincet0t15@gmail.com>
2026-01-28 15:10:48 +0800

    asdf

--- resources/js/components/app-header.tsx
+++ resources/js/components/app-header.tsx
@@ -35,7 +35,7 @@ import { dashboard } from '@/routes';
 import type { BreadcrumbItem, SharedData } from '@/types';
 import AppLogo from './app-logo';
 import AppLogoIcon from './app-logo-icon';
-import { Label } from './ui/label';
+import { NavigationMenuDemo } from './test';
 
 type Props = {
     breadcrumbs?: BreadcrumbItem[];
@@ -49,6 +49,18 @@ const mainNavItems = [
     },
 ];
 
+const rightNavItems = [
+    {
+        title: 'Repository',
+        href: 'https://github.com/laravel/laravel',
+        icon: Folder,
+    },
+    {
+        title: 'Documentation',
+        href: 'https://laravel.com/docs',
+        icon: BookOpen,
+    },
+];
 
 
 const activeItemStyles =
@@ -62,7 +74,7 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
     return (
         <>
             <div className="border-b border-sidebar-border/80">
-                <div className="mx-auto flex h-16 items-center px-4 relative">
+                <div className="mx-auto flex h-16 items-center px-4 md:max-w-7xl">
                     {/* Mobile Menu */}
                     <div className="lg:hidden">
                         <Sheet>
@@ -83,10 +95,7 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
                                     Navigation Menu
                                 </SheetTitle>
                                 <SheetHeader className="flex justify-start text-left">
-                                    <div className='flex items-center gap-2'>
-                                        <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
-                                        <Label>Starter Kit</Label>
-                                    </div>
+                                    <AppLogoIcon className="h-6 w-6 fill-current text-black dark:text-white" />
                                 </SheetHeader>
                                 <div className="flex h-full flex-1 flex-col space-y-4 p-4">
                                     <div className="flex h-full flex-col justify-between text-sm">
@@ -105,7 +114,22 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
                                             ))}
                                         </div>
 
-
+                                        <div className="flex flex-col space-y-4">
+                                            {rightNavItems.map((item) => (
+                                                <a
+                                                    key={item.title}
+                                                    href={toUrl(item.href)}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="flex items-center space-x-2 font-medium"
+                                                >
+                                                    {item.icon && (
+                                                        <item.icon className="h-5 w-5" />
+                                                    )}
+                                                    <span>{item.title}</span>
+                                                </a>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                             </SheetContent>
@@ -121,7 +145,7 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
                     </Link>
 
                     {/* Desktop Navigation */}
-                    <div className="hidden absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full items-center space-x-6 lg:flex">
+                    <div className="ml-6 hidden h-full items-center space-x-6 lg:flex">
                         <NavigationMenu className="flex h-full items-stretch">
                             <NavigationMenuList className="flex h-full items-stretch space-x-2">
                                 {mainNavItems.map((item, index) => (
@@ -151,11 +175,49 @@ export function AppHeader({ breadcrumbs = [] }: Props) {
                                     </NavigationMenuItem>
                                 ))}
                             </NavigationMenuList>
+
                         </NavigationMenu>
                     </div>
 
                     <div className="ml-auto flex items-center space-x-2">
-
+                        <div className="relative flex items-center space-x-1">
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                className="group h-9 w-9 cursor-pointer"
+                            >
+                                <Search className="!size-5 opacity-80 group-hover:opacity-100" />
+                            </Button>
+                            <div className="ml-1 hidden gap-1 lg:flex">
+                                {rightNavItems.map((item) => (
+                                    <TooltipProvider
+                                        key={item.title}
+                                        delayDuration={0}
+                                    >
+                                        <Tooltip>
+                                            <TooltipTrigger>
+                                                <a
+                                                    href={toUrl(item.href)}
+                                                    target="_blank"
+                                                    rel="noopener noreferrer"
+                                                    className="group inline-flex h-9 w-9 items-center justify-center rounded-md bg-transparent p-0 text-sm font-medium text-accent-foreground ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50"
+                                                >
+                                                    <span className="sr-only">
+                                                        {item.title}
+                                                    </span>
+                                                    {item.icon && (
+                                                        <item.icon className="size-5 opacity-80 group-hover:opacity-100" />
+                                                    )}
+                                                </a>
+                                            </TooltipTrigger>
+                                            <TooltipContent>
+                                                <p>{item.title}</p>
+                                            </TooltipContent>
+                                        </Tooltip>
+                                    </TooltipProvider>
+                                ))}
+                            </div>
+                        </div>
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
                                 <Button
