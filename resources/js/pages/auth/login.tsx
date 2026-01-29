bd7b1ab4da960b67a1c17db805e8495816eb54f9
vincet0t15 <vincet0t15@gmail.com>
2026-01-29 20:37:41 +0800

    asf

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -54,18 +54,7 @@ export default function Login({
                             </div>
 
                             <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                    {canResetPassword && (
-                                        <TextLink
-                                            href={request()}
-                                            className="ml-auto text-sm"
-                                            tabIndex={5}
-                                        >
-                                            Forgot password?
-                                        </TextLink>
-                                    )}
-                                </div>
+
                                 <Input
                                     id="password"
                                     type="password"
