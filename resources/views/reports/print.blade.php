4d157b848ae1539c99742377f813f170a7f026eb
vincet0t15 <vincet0t15@gmail.com>
2026-02-02 11:30:23 +0800

    asdf

--- resources/views/reports/print.blade.php
+++ resources/views/reports/print.blade.php
@@ -25,6 +25,13 @@
             margin-bottom: 10px;
         }
 
+        .logo-text {
+            font-size: 24px;
+            font-weight: bold;
+            text-transform: uppercase;
+            line-height: 1.2;
+        }
+
         .header h1 {
             margin: 0;
             font-size: 16px;
@@ -144,22 +151,12 @@
     <div class="header">
         <table width="100%">
             <tr>
-                <td width="20%" style="vertical-align: top; text-align: left;">
-                    @if($system_settings->logo && file_exists(public_path($system_settings->logo)))
-                    <img src="{{ public_path($system_settings->logo) }}" class="logo" alt="Logo">
-                    @elseif(file_exists(public_path('smartvote.png')))
-                    <img src="{{ public_path('smartvote.png') }}" class="logo" alt="Logo">
-                    @else
-                    {{-- Fallback SVG Logo --}}
-                    <svg width="60" height="60" viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="#059669" fill-rule="evenodd" clip-rule="evenodd" d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z" />
-                    </svg>
-                    @endif
+                <td width="20%" style="vertical-align: middle; text-align: left;">
+                    <div class="logo-text">{{ $system_settings->name ?? 'Voting System' }}</div>
                 </td>
-                <td width="60%" style="vertical-align: top; text-align: center;">
-                    <h1>{{ $system_settings->name ?? 'Voting System' }}</h1>
-                    <h2>{{ $type === 'winners' ? 'OFFICIAL LIST OF WINNERS' : 'OFFICIAL ELECTION RESULTS' }}</h2>
-                    <h3>{{ $event->name }}</h3>
+                <td width="60%" style="vertical-align: middle; text-align: center;">
+                    <h1>{{ $type === 'winners' ? 'OFFICIAL LIST OF WINNERS' : 'OFFICIAL ELECTION RESULTS' }}</h1>
+                    <h2>{{ $event->name }}</h2>
                 </td>
                 <td width="20%" style="vertical-align: top; text-align: right; font-size: 10px; color: #666;">
                     Ref: {{ $event->id }}-{{ date('Y') }}
