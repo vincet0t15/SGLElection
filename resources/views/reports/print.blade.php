28e51395cc595e4457b6f23fafed90efd0c8526b
vincet0t15 <vincet0t15@gmail.com>
2026-02-02 14:08:42 +0800

    asfasdf

--- resources/views/reports/print.blade.php
+++ resources/views/reports/print.blade.php
@@ -152,18 +152,42 @@
         <table width="100%">
             <tr>
                 <td width="10%" style="vertical-align: top; text-align: center;">
-                    @if($system_settings->logo && file_exists(public_path($system_settings->logo)))
-                    <img src="{{ public_path($system_settings->logo) }}" class="logo" alt="Logo" style="height: 60px; width: auto;">
-                    @elseif(file_exists(public_path('smartvote.png')))
-                    <img src="{{ public_path('smartvote.png') }}" class="logo" alt="Logo" style="height: 60px; width: auto;">
+                    @php
+                    $logoPath = null;
+                    if ($system_settings->system_logo) {
+                    $storagePath = \Illuminate\Support\Facades\Storage::disk('public')->path($system_settings->system_logo);
+                    if (file_exists($storagePath)) {
+                    $logoPath = $storagePath;
+                    } elseif (file_exists(public_path($system_settings->system_logo))) {
+                    $logoPath = public_path($system_settings->system_logo);
+                    }
+                    }
+
+                    $logoData = null;
+
+                    if ($logoPath && file_exists($logoPath)) {
+                    $type = pathinfo($logoPath, PATHINFO_EXTENSION);
+                    $data = file_get_contents($logoPath);
+                    $logoData = 'data:image/' . $type . ';base64,' . base64_encode($data);
+                    }
+                    @endphp
+
+                    @if ($logoData)
+                    <img
+                        src="{{ $logoData }}"
+                        alt="Logo"
+                        style="height: 60px; width: auto;">
                     @else
+                    {{-- Default SVG Logo --}}
                     <svg width="60" height="60" viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="#059669" fill-rule="evenodd" clip-rule="evenodd" d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325ZM38 18.2999L32.4 21.411V15.2545L38 12.1434V18.2999ZM36.9409 10.4439L31.4 13.5221L25.8591 10.4439L31.4 7.36561L36.9409 10.4439ZM24.8 18.2999V12.1434L30.4 15.2545V21.411L24.8 18.2999ZM23.8 20.0323L29.3409 23.1105L16.2 30.411L10.6591 27.3328L23.8 20.0323ZM7.6 27.9212L15.2 32.1434V38.2999L2 30.9666V7.92116L7.6 11.0323V27.9212ZM8.6 9.29991L3.05913 6.22165L8.6 3.14339L14.1409 6.22165L8.6 9.29991ZM30.4 24.8101L17.2 32.1434V38.2999L30.4 30.9666V24.8101ZM9.6 11.0323L15.2 7.92117V22.5221L9.6 25.6333V11.0323Z" />
+                        <path fill="#059669" fill-rule="evenodd" clip-rule="evenodd"
+                            d="M17.2 5.63325L8.6 0.855469L0 5.63325V32.1434L16.2 41.1434L32.4 32.1434V23.699L40 19.4767V9.85547L31.4 5.07769L22.8 9.85547V18.2999L17.2 21.411V5.63325Z" />
                     </svg>
                     @endif
                 </td>
+
                 <td width="70%" style="vertical-align: top; text-align: left; padding-left: 10px;">
-                    <div style="font-weight: bold; font-size: 14px; text-transform: uppercase; font-family: 'Times New Roman', serif;">{{ $system_settings->name ?? 'Voting System' }}</div>
+                    <div style="font-weight: bold; font-size: 14px; text-transform: uppercase; font-family: 'Times New Roman', serif;">{{ $system_settings->system_name ?? 'Voting System' }}</div>
                     <div style="font-weight: bold; font-size: 12px; margin-top: 2px; text-transform: uppercase;">{{ $type === 'winners' ? 'OFFICIAL LIST OF WINNERS' : 'OFFICIAL ELECTION RESULTS' }}</div>
                     <div style="font-size: 12px; margin-top: 2px; text-transform: uppercase;">{{ $event->name }}</div>
                 </td>
