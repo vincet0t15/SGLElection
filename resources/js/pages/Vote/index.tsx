20ab100dd6a1dbe3b99f29ad9264a8d23730698e
vincet0t15 <vincet0t15@gmail.com>
2026-02-06 17:31:51 +0800

    asdf

--- resources/js/pages/Vote/index.tsx
+++ resources/js/pages/Vote/index.tsx
@@ -268,7 +268,7 @@ export default function VoteIndex({ events }: Props) {
                                     </div>
 
 
-                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5">
                                         {position.candidates.map((candidate) => {
                                             const selected = isSelected(position.id, candidate.id);
                                             const photoUrl = candidate.candidate_photos && candidate.candidate_photos.length > 0
